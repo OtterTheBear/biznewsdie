ca921062eec9f1ff5b0975c82ed54f6732adda6a
Younis Latyp <younislatyp@gmail.com>
2021-07-17 03:05:46 -0400

    Add code to make ansi codes on windows work
    Check if posix before including termios.h. Change the getch function's
    name to getchr because of windows errors about getch.

--- biznewsdie.c
+++ biznewsdie.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 #include <termios.h>
+#endif
 #ifdef _WIN32
 #include <conio.h>
+#include <windows.h>
 #endif
 
+int getchr() {
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
-int getch() {
     return getchar();
-}
+#elif defined (_WIN32)
+    return _getch();
 #endif
+}
 
 char *biznews() {
-    char *results[] = {"tech",  "food", "entertainment", "sports", "tourism", "biznews"};
+    char *results[] = {"tech", "food", "entertainment", "sports", "tourism", "biznews"};
     int the_num = rand() % 6;
     return results[the_num];
 }
 
 int main() {
+#ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+#endif
     srand(time(NULL));
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 #define STDIN_FILENO 0
@@ -31,6 +44,7 @@ int main() {
 #endif
     do {
         printf("\x1b[2K\x1b[G%s", biznews());
-    } while (getchar());
+    } while (getchr());
     return 0;
 }
+
