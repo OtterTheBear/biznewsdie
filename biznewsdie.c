8514f2ff7318934c8cba0777a07537d05e401644
Younis Latyp <younislatyp@gmail.com>
2021-07-17 02:05:39 -0400

    Add an all-in-one program

---
+++ biznewsdie.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <termios.h>
+#ifdef _WIN32
+#include <conio.h>
+#endif
+
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+int getch() {
+    return getchar();
+}
+#endif
+
+char *biznews() {
+    char *results[] = {"tech",  "food", "entertainment", "sports", "tourism", "biznews"};
+    int the_num = rand() % 6;
+    return results[the_num];
+}
+
+int main() {
+    srand(time(NULL));
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+#define STDIN_FILENO 0
+    struct termios told, tnew;
+    tcgetattr(STDIN_FILENO, &told);
+    tnew = told;
+    tnew.c_lflag &= (~ECHO & ~ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &tnew);
+#undef STDIN_FILENO
+#endif
+    do {
+        printf("\x1b[2K\x1b[G%s", biznews());
+    } while (getchar());
+    return 0;
+}
